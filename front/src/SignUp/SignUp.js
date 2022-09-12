37872422d20d48d469948385aec514d755e80830
Arkadii Shyn <arkadiy.shin@gmail.com>
2022-09-12 12:49:30 +0200

    Created a registration form

--- front/src/SignUp/SignUp.js
+++ front/src/SignUp/SignUp.js
@@ -2,12 +2,34 @@ import { useState } from "react";
 
 
 function SignUp() {
-const [email, setEmail] = useState('test@test.com');
+    
+    const [inputState, setInputState] = useState({});
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(inputState);
+    }
+
+    const changeInputHandler = (event) => {
+        setInputState( prev => ({...prev, ...{ 
+            [event.target.name]: event.target.value 
+        }}))
+    }
+
     return (
-        <>
-            <h1>{email}</h1>
-            <input type="email" name="email" value={email} onChange={ (e) => setEmail(e.target.value)}/>
-        </>
+        <form onSubmit={submitHandler}>
+            
+            <h1>{JSON.stringify(inputState,1,1)}</h1>
+
+            <input type="email" name="email" value={inputState.email} onChange={changeInputHandler} />
+            <input type="password" name="password" value={inputState.password} onChange={changeInputHandler} />
+            <input type="password" name="passwordconf" value={inputState.passwordconf} onChange={changeInputHandler} />
+            <input type="text" name="name" value={inputState.name} onChange={changeInputHandler} />
+            <input type="text" name="lastname" value={inputState.lastname} onChange={changeInputHandler} />
+            
+            <input type="submit" value="Submit" />
+
+        </form>
     )
 }
 
