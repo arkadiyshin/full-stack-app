01986977301a3ff58a8ff62bb8a0183d0b09d87c
Arkadii Shyn <arkadiy.shin@gmail.com>
2022-09-12 21:07:56 +0200

    Register an account in a database

---
+++ back/helpers/db.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql';
+import {} from 'dotenv/config';
+
+console.log(process.env.DB_HOST);
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+export default connection;
