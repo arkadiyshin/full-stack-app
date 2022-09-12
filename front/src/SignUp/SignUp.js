d7fc89aaca607130b8fee108a2031d85201eb26b
Arkadii Shyn <arkadiy.shin@gmail.com>
2022-09-12 23:37:14 +0200

    050 - Your first client-server connection - front

--- front/src/SignUp/SignUp.js
+++ front/src/SignUp/SignUp.js
@@ -1,32 +1,55 @@
 import { useState } from "react";
+import axios from 'axios';
 
 
 function SignUp() {
-    
+
     const [inputState, setInputState] = useState({});
 
     const submitHandler = (event) => {
+
         event.preventDefault();
-        console.log(inputState);
+
+        // delete passwordconf form body request
+        const { passwordconf, flash, ...data } = inputState;
+
+        axios.post('/auth/signup', data)
+            .then(res => {
+                setInputState(prev => ({
+                    ...prev, ...{
+                        "flash": res.data.flash
+                    }
+                }))
+            })
+            .catch(err => {
+                setInputState(prev => ({
+                    ...prev, ...{
+                        "flash": err.data.flash
+                    }
+                }))
+            })
+
     }
 
     const changeInputHandler = (event) => {
-        setInputState( prev => ({...prev, ...{ 
-            [event.target.name]: event.target.value 
-        }}))
+        setInputState(prev => ({
+            ...prev, ...{
+                [event.target.name]: event.target.value
+            }
+        }))
     }
 
     return (
         <form onSubmit={submitHandler}>
-            
-            <h1>{JSON.stringify(inputState,1,1)}</h1>
+
+            <h1>{JSON.stringify(inputState, 1, 1)}</h1>
 
             <input type="email" name="email" value={inputState.email} onChange={changeInputHandler} />
             <input type="password" name="password" value={inputState.password} onChange={changeInputHandler} />
             <input type="password" name="passwordconf" value={inputState.passwordconf} onChange={changeInputHandler} />
             <input type="text" name="name" value={inputState.name} onChange={changeInputHandler} />
             <input type="text" name="lastname" value={inputState.lastname} onChange={changeInputHandler} />
-            
+
             <input type="submit" value="Submit" />
 
         </form>
